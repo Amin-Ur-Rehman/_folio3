f31335d107d5f49a574eff9e932ea690f30ddb97
smehmoodf3 <smehmood@folio3.com>
2015-02-10 12:45:53 +0500

    commit

--- scheduled/customer_savePwInField_sch.js
+++ scheduled/customer_savePwInField_sch.js
@@ -1,54 +1,45 @@
 var ScheduledScriptConstant = {
     Minutes: 15,
-    RemainingUsage: 1000,
+    RemainingUsage: 100,
     StartTime: (new Date()).getTime()
 };
 
-
-
-
-function startup()
-{
-    var arrFils=[];
-    var arrCols=[];
+function startup() {
+    var arrFils = [];
+    var arrCols = [];
     var magentoIdFieldValue;
     var magentoInfoJSon;
-    var storeId='1';
-    var CUSTOMER_STATUSES=ConnectorConstants.CustomerTypesToExport;
+    var storeId = '1';
+    var CUSTOMER_STATUSES = ConnectorConstants.CustomerTypesToExport;
 
-    arrFils.push(new nlobjSearchFilter('entitystatus', null, 'anyof',CUSTOMER_STATUSES));
+    arrFils.push(new nlobjSearchFilter('entitystatus', null, 'anyof', CUSTOMER_STATUSES));
 
     arrFils.push(new nlobjSearchFilter('custentity_magentosync_dev', null, 'is', 'T'));
 
-    arrFils.push(new nlobjSearchFilter('custentity_magentopw', null, 'is','empty'));
+    arrFils.push(new nlobjSearchFilter('custentity_magentopw', null, 'isempty'));
 
     arrCols.push(new nlobjSearchColumn('custentity_magento_custid'));
 
 
     var recs = nlapiSearchRecord('customer', null, arrFils, arrCols);
 
-    if (recs != null && recs.length > 0) {
+    if (recs !== null && recs.length > 0) {
 
         for (var i = 0; i < recs.length; i++) {
 
             magentoIdFieldValue = recs[i].getValue('custentity_magento_custid');
 
+            if (!!magentoIdFieldValue) {
+                magentoInfoJSon = JSON.parse(magentoIdFieldValue);
 
-            if(!!magentoIdFieldValue)
-            {
-                magentoInfoJSon=JSON.parse(magentoIdFieldValue);
-
-                if (!!magentoInfoJSon && magentoInfoJSon.length > 0)
-                {
-                    for (var i = 0; i < magentoInfoJSon.length; i++)
-                    {
-                        if(magentoInfoJSon[i].StoreId === storeId)
-                        {
-                            if(!!magentoInfoJSon[i].Password) {
+                if (!!magentoInfoJSon && magentoInfoJSon.length > 0) {
+                    for (var j = 0; j < magentoInfoJSon.length; i++) {
+                        if (magentoInfoJSon[i].StoreId === storeId) {
+                            if (!!magentoInfoJSon[j].Password) {
 
                                 nlapiSubmitField('customer', recs[i].getId(), 'custentity_magentopw', magentoInfoJSon[i].Password);
 
-                                nlapiLogExecution('debug','Customer ID',recs[i].getId() );
+                                nlapiLogExecution('debug', 'Customer ID', recs[i].getId());
 
                                 return;
                             }
@@ -69,7 +60,6 @@ function startup()
 }
 
 
-
 // check if the script is required to be scheduled
 function rescheduleIfRequired(params) {
     var context = nlapiGetContext();
@@ -80,12 +70,12 @@ function rescheduleIfRequired(params) {
     minutes = Math.round(((endTime - ScheduledScriptConstant.StartTime) / (1000 * 60)) * 100) / 100;
 
     if (context.getRemainingUsage() < ScheduledScriptConstant.RemainingUsage) {
-        //nlapiScheduleScript(context.getScriptId(), context.getDeploymentId(), params);
+        nlapiScheduleScript(context.getScriptId(), context.getDeploymentId(), params);
         return true;
     }
 
     if (minutes > ScheduledScriptConstant.Minutes) {
-        //nlapiScheduleScript(context.getScriptId(), context.getDeploymentId(), params);
+        nlapiScheduleScript(context.getScriptId(), context.getDeploymentId(), params);
         return true;
     }
 
