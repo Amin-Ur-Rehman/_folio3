2d53206602c6f05807733da6a33c6d8289885fdf
Sameer Ahmed <sameerdgrea8@gmail.com>
2015-08-31 11:37:33 +0500

    Adding Gift certificate Lib

---
+++ util/f3mg_gift_certificate_lib.js
@@ -0,0 +1,25 @@
+/**
+ * Created by sameer on 8/31/15.
+ * TODO:
+ * -
+ * Referenced By:
+ * -
+ * -
+ * Dependencies:
+ * -
+ * -
+ */
+
+/**
+ * giftCertificate class that has the functionality of
+ */
+var giftCertificate = (function () {
+    return {
+        /**
+         * Init method
+         */
+        initialize: function () {
+
+        }
+    };
+})();
