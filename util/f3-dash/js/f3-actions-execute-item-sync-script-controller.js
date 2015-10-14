bfa839d230ef0688af45bb4f47622088d6230347
Zain Shaikh <zshaikh@folio3.com>
2015-10-14 16:48:40 +0500

    separated some action from dashboard.min.js file implemented view logs screen.

---
+++ util/f3-dash/js/f3-actions-execute-item-sync-script-controller.js
@@ -0,0 +1,35 @@
+/**
+ * Created by zshaikh on 10/8/2015.
+ */
+
+(function() {
+
+    'use strict';
+
+    angular.module("f3UC")
+        .controller("ExecuteItemSyncScriptController", ExecuteItemSyncScriptController);
+
+
+    // TODO : need to implement inheritance to prevent duplicate code.
+    // TODO : we should also consider moving server calls into separate angular services
+
+    function ExecuteItemSyncScriptController(f3StoreId, $http) {
+        console.log('ExecuteItemSyncScriptController');
+
+        var viewModel = this;
+
+        this.execute = function () {
+            viewModel.showLoadingIcon = true;
+
+            var apiUrl = location.href.replace(location.hash, '') + '&method=executeItemSyncScript';
+            $http.get(apiUrl)
+                .success(function(response) {
+                    viewModel.successMessage = response.success;
+                    viewModel.errorMessage = response.error;
+                    viewModel.showLoadingIcon = false;
+                });
+        };
+    }
+
+
+})();
